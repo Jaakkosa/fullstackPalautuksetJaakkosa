c48356406ca05038cc861ea0f786a016e42a4d74
Jaakkosa <jaakko.satuli@gmail.com>
2022-07-27 15:28:11 +0300

    push

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,15 +3,24 @@ import Numero from "./Numero"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name:'Arto Hellas'}
+    { name:'Arto Hellas',
+  number: 12345645322}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newRajaus, setNewRajaus] = useState('')
+  const [näytetäänkö, setNäytetäänkö] = useState(true)
+
+
+ 
 
   const addNew = (event) => {
 event.preventDefault()
 const Nimi = {
-  name: newName
-}
+  name: newName,
+  number: newNumber
+} 
+
 const löytyykö = (henkilö) => henkilö.name === Nimi.name
 if(persons.some(löytyykö)){
   alert(`${Nimi.name} allready added`)
@@ -19,26 +28,55 @@ if(persons.some(löytyykö)){
 else{
 setPersons(persons.concat(Nimi))
 setNewName('')
+setNewNumber('')
 }
   }
 const sisällönTutkiminen = (event) => {
 setNewName(event.target.value)
 }
 
+const numeronTutkiminen = (event) => {
+  setNewNumber(event.target.value)
+  
+}
+
+ 
+
+const rajauksenTutkiminen = (event) => {
+  setNewRajaus(event.target.value)
+  const kuuluuko = (henkilö) => henkilö.name.toLowerCase().includes(newRajaus.toLowerCase())
+  console.log(newRajaus.toLowerCase())
+
+
+ setNäytetäänkö(persons.some(kuuluuko))
+}
+
+const RajausToShow = näytetäänkö
+? persons.filter(note => note.name.toLowerCase().includes(newRajaus.toLowerCase()))
+: persons.filter(note => note.name.toLowerCase().includes(newRajaus.toLowerCase()))
+
+
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value = {newRajaus} onChange = {rajauksenTutkiminen} />
+      </div>
       <form onSubmit={addNew}>
         <div>
           name: <input value = {newName} onChange = {sisällönTutkiminen} />
         </div>
         <div>
+          number: <input value = {newNumber} onChange = {numeronTutkiminen}/>
+          </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-{persons.map(person => <Numero key={person.name} name={person.name}/> )}
+{RajausToShow.map(person => <Numero key={person.name} name={person.name} number={person.number}/> )}
     </div>
   )
 
--- osa2/puhelinluettelo/src/Numero.js
+++ osa2/puhelinluettelo/src/Numero.js
@@ -1,6 +1,6 @@
 const Numero = (props) => {
     return (
-      <li>{props.name}</li>
+      <li>{props.name} {props.number}</li>
     )
   }
   
